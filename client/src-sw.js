19f67b04d231fa3760a5fa4c17a3aff76cc6d7b6
CameronG7 <122698132+CameronG7@users.noreply.github.com>
2023-07-02 16:32:07 -0700

    all not cache fix

--- client/src-sw.js
+++ client/src-sw.js
@@ -6,30 +6,8 @@ const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
 precacheAndRoute(self.__WB_MANIFEST);
-
-const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
-  plugins: [
-    new CacheableResponsePlugin({
-      statuses: [0, 200],
-    }),
-    new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
-    }),
-  ],
-});
-
-warmStrategyCache({
-  urls: ['/index.html', '/'],
-  strategy: pageCache,
-});
-
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
-
-// TODO: Implement asset caching
 registerRoute(
   ({ request }) => {
-    console.log(request);
     return (
       // CSS
       request.destination === 'style' ||
@@ -37,7 +15,7 @@ registerRoute(
       request.destination === 'script'
     );
   },
-  new StaleWhileRevalidate({
+  new CacheFirst({
     cacheName: 'static-resources',
     plugins: [
       new CacheableResponsePlugin({
@@ -61,3 +39,24 @@ registerRoute(
     ],
   })
 );
+
+const pageCache = new CacheFirst({
+  cacheName: 'page-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+});
+
+warmStrategyCache({
+  urls: ['/index.html', '/'],
+  strategy: pageCache,
+});
+
+registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+
+// TODO: Implement asset caching
